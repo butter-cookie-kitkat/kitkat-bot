92ca04bc55776800b10c576ce09cd22b7e3fbdff
Nick Woodward <nick@salte.io>
2020-04-23 16:09:34 -0500

    fix: potential stack overflow issue

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import 'dotenv/config.js';
 import { client } from './src/utils/discord.js';
+import * as Wait from './src/utils/wait.js';
 import { servers } from './src/config/servers.js';
-import { periodicallyRefreshStatuses } from './src/services/server.js';
+import { refreshStatuses } from './src/services/server.js';
 import { ProcessCommand } from './src/services/commands.js';
 
 client.on('ready', async () => {
     console.log('Kitkat Bot initialized successfully!');
 
-    await periodicallyRefreshStatuses(process.env.ANNOUNCEMENTS_CHANNEL_ID, servers);
+    while (true) {
+        await Promise.all([
+            refreshStatuses(process.env.ANNOUNCEMENTS_CHANNEL_ID, servers),
+            Wait.wait(60000)
+        ]);
+    }
 });
 
 client.on('message', ProcessCommand);
--- src/services/server.js
+++ src/services/server.js
@@ -5,53 +5,44 @@ import * as Status from '../utils/status.js';
 
 import { ping } from '../utils/ping.js';
 
-export async function periodicallyRefreshStatuses(channelID, servers, delay = 60000) {
-    try {
-        await Promise.all([
-            new Promise((resolve) => setTimeout(resolve, delay)),
-            refreshStatuses(channelID, servers)
-        ]);
-    } catch (error) {
-        console.error('Failed to refresh statuses', error);
-    }
-
-    periodicallyRefreshStatuses(channelID, servers, delay);
-}
-
 export async function refreshStatuses(channelID, servers) {
     console.log('Refreshing statuses...');
 
-    const channel = await client.channels.fetch(channelID);
+    try {
+        const channel = await client.channels.fetch(channelID);
+    
+        const messages = await channel.messages.fetch();
+    
+        const existingMessage = messages.find((message) => message.author.id === client.user.id && message.content.includes('`kitkat-bot.servers.refresh`'));
+    
+        const responses = await Promise.all(servers.map(async ({ name, address }) => ({
+            name,
+            address,
+            status: await ping(address)
+        })));
+    
+        const content = dedent`
+            **Servers**
+    
+            ${responses.map(({ name, address, status }) => dedent`
+                > _**${name}**_
+                > 
+                > Connection Info: _${address}_
+                > Status: ${Status.available(status)}
+                > Response Time: ${Status.responseTime(status)}
+            `)}
+    
+            \`kitkat-bot.servers.refresh\`
+        `;
+    
+        if (existingMessage) {
+            await existingMessage.edit(content);
+        } else {
+            await channel.send(content);
+        }
 
-    const messages = await channel.messages.fetch();
-
-    const existingMessage = messages.find((message) => message.author.id === client.user.id && message.content.includes('`kitkat-bot.servers.refresh`'));
-
-    const responses = await Promise.all(servers.map(async ({ name, address }) => ({
-        name,
-        address,
-        status: await ping(address)
-    })));
-
-    const content = dedent`
-        **Servers**
-
-        ${responses.map(({ name, address, status }) => dedent`
-            > _**${name}**_
-            > 
-            > Connection Info: _${address}_
-            > Status: ${Status.available(status)}
-            > Response Time: ${Status.responseTime(status)}
-        `)}
-
-        \`kitkat-bot.servers.refresh\`
-    `;
-
-    if (existingMessage) {
-        await existingMessage.edit(content);
-    } else {
-        await channel.send(content);
+        console.log('Statuses refreshed successfully!');
+    } catch (error) {
+        console.error('Failed to refresh statuses', error);
     }
-
-    console.log('Statuses refreshed successfully!');
 }
