fcc854b8b9f91a47cf86530c0d508653a9e213db
Nick Woodward <nick@salte.io>
2020-06-19 19:28:02 -0500

    chore(tests): separated unit and integration tests

--- bot/services/xivapi/__test__/core.spec.ts
+++ bot/services/xivapi/__test__/core.spec.ts
@@ -2,7 +2,6 @@ import { expect } from 'chai';
 
 import { xivapi } from '../index';
 import { CONFIG } from '../../../config';
-import { buildUrl } from '../../../utils/fetch';
 
 describe('Service(XIVAPI.Core)', () => {
   describe('func(url)', () => {
@@ -20,60 +19,6 @@ describe('Service(XIVAPI.Core)', () => {
 
     it('should ignore the live status for maps', () => {
       expect(xivapi.core.url('/m/some-map.png', false)).equals('https://xivapi.com/m/some-map.png');
-    });
-  });
-
-  describe('func(getPage)', () => {
-    it('should retrieve page 1 by default', async () => {
-      const items = await xivapi.core.getPage('/GatheringItem');
-
-      expect(items.Pagination.Results).equals(100);
-      expect(items.Results).length(items.Pagination.Results);
-    });
-
-    it('should retrieve whatever page is specified', async () => {
-      const expectedPage = 3;
-      const items = await xivapi.core.getPage('/GatheringItem', expectedPage);
-
-      expect(items.Pagination.Page).equals(expectedPage);
-      expect(items.Pagination.Results).equals(100);
-      expect(items.Results).length(items.Pagination.Results);
-    });
-
-    it('should support invalid ids', async () => {
-      const items = await xivapi.core.getPage(buildUrl('/GatheringItem', {
-        ids: ['abc'],
-      }));
-
-      expect(items.Results).deep.equals([{
-        ID: 0,
-        Icon: null,
-        Name: null,
-        Url: '/GatheringItem/0',
-      }]);
-    });
-  });
-
-  describe('func(getAllPages)', () => {
-    it('should retrieve all pages for a given endpoint', async () => {
-      const { Pagination } = await xivapi.core.getPage('/GatheringItem');
-
-      const items = await xivapi.core.getAllPages('/GatheringItem');
-
-      expect(items).length(Pagination.ResultsTotal);
-    });
-
-    it('should support invalid ids', async () => {
-      const items = await xivapi.core.getAllPages(buildUrl('/GatheringItem', {
-        ids: ['abc'],
-      }));
-
-      expect(items).deep.equals([{
-        ID: 0,
-        Icon: null,
-        Name: null,
-        Url: '/GatheringItem/0',
-      }]);
     });
   });
 });
--- bot/utils/__test__/arrays.spec.ts
+++ bot/utils/__test__/arrays.spec.ts
@@ -14,4 +14,14 @@ describe('Utils(Arrays)', () => {
       expect(arrays.flatten([[1, 2], [3, 4], 5])).deep.equals([1, 2, 3, 4, 5]);
     });
   });
+
+  describe('func(unique)', () => {
+    it('should dedupe values', () => {
+      expect(arrays.unique([1, 2, 3, 2, 2])).deep.equals([1, 2, 3]);
+    });
+
+    it('should support a custom identifier', () => {
+      expect(arrays.unique([{ id: 1 }, { id: 1 }], (item) => item.id)).deep.equals([{ id: 1 }]);
+    });
+  });
 });
